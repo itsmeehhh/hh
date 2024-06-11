4151e9f666c04ae1a4221567bfc4638bdbafb44b
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-12 00:33:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-import { firefox } from 'playwright'; 
-console.log("watching");
+import { firefox } from 'playwright'; // استيراد مكتبة بلاي رايت لمتصفح فايرفوكس
+
+console.log("watching"); // طباعة رسالة في الكونسول
+
 function openPage() {
   return new Promise(async (resolve, reject) => {
-    
-    const browser = await firefox.launch({ headless: true  });
-    const page = await browser.newPage(); 
-    await page.goto('https://youtu.be/qIz9GgkWyts?si=ethiWCZccRy78v_I');
-    await page.click('button[aria-label="Play"]'); 
-    await page.waitForTimeout(120000);
-    await browser.close();
-    resolve();
+    let browser;
+    try {
+      browser = await firefox.launch({ headless: true }); // إطلاق المتصفح بالوضع الخفي
+      const page = await browser.newPage(); // فتح صفحة جديدة
+      await page.goto('https://youtu.be/qIz9GgkWyts'); // الانتقال إلى الرابط المحدد
+      await page.click('button[aria-label="Play"]'); // النقر على زر التشغيل
+      await page.waitForTimeout(120000); // الانتظار لمدة دقيقتين
+      resolve();
+    } catch (error) {
+      console.error('حدث خطأ أثناء فتح الصفحة:', error);
+      reject(error);
+    } finally {
+      if (browser) {
+        await browser.close(); // تأكد من إغلاق المتصفح حتى في حالة حدوث خطأ
+      }
+    }
   });
 }
 
@@ -18,7 +28,9 @@ async function executeInParallel() {
   for (let i = 0; i < 5; i++) {
     promises.push(openPage());
   }
-  await Promise.all(promises);
+  await Promise.all(promises).catch(error => {
+    console.error('حدث خطأ أثناء تنفيذ الوعود بالتوازي:', error);
+  });
 }
 
 async function repeatForever() {
