477d7975657f46c7e34d66d095c20843f447ada8
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-15 22:09:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import { firefox } from 'playwright';
 import UserAgent from 'user-agents';
-import ytdl from 'ytdl-core';
-
 let url = "https://m.youtube.com/watch?v=u5j85Z7EMuM";
-const videoInfo = await ytdl.getBasicInfo(url);
-const videoDuration = parseInt(videoInfo.videoDetails.lengthSeconds) + 10;
 console.log(`watching: ${url}`);
-let timewatch = videoDuration;
 
 async function openPage() {
   return new Promise(async (resolve, reject) => {
@@ -15,13 +10,12 @@ async function openPage() {
       browser = await firefox.launch({ headless: true });
       const context = await browser.newContext();
       const page = await context.newPage();
-      await page.goto(url, { timeout: 0 });
+      await page.goto(url, { timeout: 60000 });
       await page.click('button[aria-label="Play"]');
-      console.log('clicked');
-      await page.waitForTimeout(videoDuration);
+      await page.waitForTimeout(120000);
       resolve();
     } catch (error) {
-      console.error('error b:', error);
+      console.error('error:', error);
       reject(error);
     } finally {
       if (browser) {
