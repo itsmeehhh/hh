14d36c3161c926544f258cf9fa1f3359475d9883
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-15 16:57:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function openPage(cookie) {
 
 async function executeInParallel() {
   const promises = [];
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     promises.push(openPage(cookiesArray[i]));
   }
   await Promise.all(promises).catch(error => {
