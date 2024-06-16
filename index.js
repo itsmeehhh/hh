5809b666b74e99d723c9d589de7453d51155597e
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-16 02:58:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import { fork } from 'child_process';
 import { firefox } from 'playwright';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function openBrowserAndPlayVideo() {
   const browser = await firefox.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto('https://m.youtube.com/watch?v=u5j85Z7EMuM');
-  console.log('1');
+  
   // Click the play button if it exists
   const playButton = await page.$('button[aria-label="Play"]');
   if (playButton) {
     await playButton.click();
-console.log('2');
   }
   
   // Close the browser after a minute
@@ -20,7 +24,7 @@ console.log('2');
 }
 
 for (let i = 0; i < 5; i++) {
-  fork(import.meta.url, ['child'], { execArgv: [] });
+  fork(path.join(__dirname, 'index.js'), ['child'], { execArgv: [] });
 }
 
 if (process.argv[2] === 'child') {
