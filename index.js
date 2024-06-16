fa9aeb0e42e2647926fa1544f18a69b58ced2346
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-16 02:41:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,112 +1,46 @@
-import { fileURLToPath } from 'url';
-import fs from 'fs';
-import { spawn } from 'child_process';
-import express from 'express';
-const app = express();
-import { createServer } from 'http';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { exec } from 'child_process';
-import fetch from 'node-fetch';
-import { dirname } from 'path';
+import { firefox } from 'playwright';
 import UserAgent from 'user-agents';
-import { Server } from 'socket.io';
+let url = "https://fb.com";
+console.log(`watching: ${url}`);
 
-const server = createServer();
-const io = new Server(server);
-
-// تحويل URL الملف الحالي إلى مسار ملف
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const folderName = 'database';
-const children = {};
-const logs = {};
-
-//وظائف تشغيل الملفات 
-function runFile(file) {
-  const filePath = `./${folderName}/${file}`;
-  const child = spawn('node', [filePath]);
-  logs[file] = [];
-  child.stdout.on('data', (data) => {
-    const message = data.toString();
-    if (!logs[file]) {
-      logs[file] = [];
-    }
-    logs[file].push(message);
-    console.log(`${file} stdout: ${message}`);
-  });
-  child.stderr.on('data', (data) => {
-    const message = data.toString();
-    if (!logs[file]) {
-      logs[file] = [];
-    }
-    console.error(`${file} stderr: ${message}`);
-    logs[file].push(message);
-    if (message.includes('Cannot find module')) {
-      const moduleName = message.match(/'([^']+)'/)[1];
-      stopFile(file);
-      installModule(moduleName, () => {
-        runFile(file);
-      });
-    }
-  });
-  child.on('close', (code) => {
-    console.log(`${file}: exited with code ${code}`);
-    io.emit('newLog', { file, message: `Process exited with code ${code}` });
-  });
-  children[file] = child;
-}
-
-// وظيفة لإيقاف تشغيل الملف
-function stopFile(file) {
-  const child = children[file];
-  if (child) {
-    child.kill('SIGINT');
-  }
-  delete children[file];
-  delete logs[file];
-}
-
-// وظيفة لحذف كل السجلات كل 5 دقائق
-setInterval(() => {
-  Object.keys(logs).forEach((file) => {
-    delete logs[file];
-  });
-}, 300000);
-
-// تنفيذ تثبيت الوحدة
-function installModule(moduleName, callback) {
-  exec(`npm install ${moduleName}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`Error installing module ${moduleName}: ${stderr}`);
-      return;
-    }
-    console.log(`Module ${moduleName} installed successfully: ${stdout}`);
-    callback();
-  });
-}
-
-// قراءة الملفات في المجلد وتشغيلها
-fs.readdir(folderName, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  files.forEach(runFile);
-});
-
-// مراقبة المجلد للتغييرات
-fs.watch(folderName, (eventType, filename) => {
-  if (eventType === 'rename') {
-    fs.access(`./${folderName}/${filename}`, (err) => {
-      if (err) {
-        console.log(`File ${filename} was deleted`);
-        stopFile(filename);
-      } else {
-        console.log(`File ${filename} was added`);
-        runFile(filename);
+async function openPage() {
+  return new Promise(async (resolve, reject) => {
+    let browser;
+    try {
+      browser = await firefox.launch({ headless: true });
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto(url, { timeout: 60000 });
+      await page.click('button[aria-label="Play"]');
+      await page.waitForTimeout(120000);
+      resolve();
+    } catch (error) {
+      console.error('error:', error);
+      reject(error);
+    } finally {
+      if (browser) {
+        await browser.close();
       }
-    });
+    }
+  });
+}
+
+async function executeInParallel() {
+  const promises = [];
+  for (let i = 0; i < 10; i++) {
+    const userAgent = new UserAgent();
+    promises.push(openPage(userAgent.toString()));
   }
-});
+  await Promise.all(promises).catch(error => {
+    console.error('error run the codes :', error);
+  });
+}
+
+async function repeatForever() {
+  while (true) {
+    await executeInParallel();
+    console.log(`watching again : ${url}`);
+  }
+}
+
+repeatForever();
