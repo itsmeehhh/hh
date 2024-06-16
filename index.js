62852dd39211abbd100abd3f80607779baf804a8
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-16 19:09:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 import { firefox } from 'playwright';
 import UserAgent from 'user-agents';
 
-
 let url = "https://m.youtube.com/watch?v=u5j85Z7EMuM";
 console.log(`watching: ${url}`);
-let timewatch = 20000;
-const userAgent = new UserAgent();
+const userAgent = new UserAgent({ deviceCategory: 'desktop' });
     
-      const browser = await firefox.launch({ headless: true });
-      const context = await browser.newContext(userAgent);
-      console.log(userAgent);
-      const page = await context.newPage();
-      await page.goto(url, { timeout: 0 });
-      console.log('gonne');
-     try {
-      await page.click('button[aria-label="Play"]');
-      console.log('clicked');
-     } catch (e) {
-       console.log('no clicked');
-     }
-        setTimeout(
-            browser.close,
-            60000)
-    
+const browser = await firefox.launch({ headless: true });
+const context = await browser.newContext({ userAgent: userAgent.toString() });
+console.log(userAgent.toString());
+const page = await context.newPage();
+await page.goto(url, { timeout: 0 });
+console.log('gone');
+try {
+  await page.click('button[aria-label="Play"]');
+  console.log('clicked');
+} catch (e) {
+  console.log('not clicked');
+}
+
+// Wait for 1 minute
+await new Promise(resolve => setTimeout(resolve, 60000));
+
+await browser.close();
