1a453f1a7ad18e06b6a1b20bbbfe5c146509c59a
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-16 12:29:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   // أفتح المتصفح
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
 
   // اذهب الى رابط الفيديو على اليوتيوب
