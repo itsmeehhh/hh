b104d56e8dc3b2ee06837bb5b637e1b2dab5ff99
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-16 19:16:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
 import { firefox } from 'playwright';
 import UserAgent from 'user-agents';
 
-const url = "https://m.youtube.com/watch?v=u5j85Z7EMuM";
-console.log(`watching: ${url}`);
+// تعريفات
+const URL = "https://m.youtube.com/watch?v=u5j85Z7EMuM";
+const BROWSERS_COUNT = 5;
+const WATCH_DURATION_SECONDS = 60; // مدة الانتظار بالثانية
 
-const browsersCount = 5;
-const browserInstances = [];
+console.log(`watching: ${URL}`);
 
-for (let i = 0; i < browsersCount; i++) {
-  browserInstances.push((async () => {
-    const userAgent = new UserAgent({ deviceCategory: 'desktop' });
-    const browser = await firefox.launch({ headless: true });
-    const context = await browser.newContext({ userAgent: userAgent.toString() });
-    console.log(`Browser ${i+1} userAgent: ${userAgent.toString()}`);
-    const page = await context.newPage();
-    await page.goto(url, { timeout: 0 });
-    console.log(`Browser ${i+1} gone`);
-    try {
-      await page.click('button[aria-label="Play"]');
-      console.log(`Browser ${i+1} clicked`);
-    } catch (e) {
-      console.log(`Browser ${i+1} not clicked`);
-    }
-    
-    // Wait for 1 minute
-    await new Promise(resolve => setTimeout(resolve, 60000));
-    
-    await browser.close();
-  })());
+async function openBrowsers() {
+  const browserInstances = [];
+
+  for (let i = 0; i < BROWSERS_COUNT; i++) {
+    browserInstances.push((async () => {
+      const userAgent = new UserAgent({ deviceCategory: 'desktop' });
+      const browser = await firefox.launch({ headless: true });
+      const context = await browser.newContext({ userAgent: userAgent.toString() });
+      console.log(`Browser ${i+1} userAgent: ${userAgent.toString()}`);
+      const page = await context.newPage();
+      await page.goto(URL, { timeout: 0 });
+      console.log(`Browser ${i+1} gone`);
+      try {
+        await page.click('button[aria-label="Play"]');
+        console.log(`Browser ${i+1} clicked`);
+      } catch (e) {
+        console.log(`Browser ${i+1} not clicked`);
+      }
+      
+      // Wait for the specified duration in milliseconds
+      await new Promise(resolve => setTimeout(resolve, WATCH_DURATION_SECONDS * 1000));
+      
+      await browser.close();
+    })());
+  }
+
+  await Promise.all(browserInstances);
+  console.log('All browsers have finished their tasks');
+  
+  // إعادة فتح المتصفحات
+  openBrowsers();
 }
 
-Promise.all(browserInstances).then(() => {
-  console.log('All browsers have finished their tasks');
-});
+// بدء العملية
+openBrowsers();
