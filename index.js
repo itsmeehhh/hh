6e1c775d07bb95a57d05d4745de32eda4d650239
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-16 15:51:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
 import { firefox } from 'playwright';
 import UserAgent from 'user-agents';
 
-const url = 'https://fb.com'; // URL to open
-const numberOfBrowsers = 5; // Number of browsers to open
-const timeout = 60; // Timeout in seconds
+let url = "https://fb.com";
+console.log(`watching: ${url}`);
 
-function generateUserAgent(osType) {
-  return new UserAgent(osType).toString();
-}
-
-async function openBrowser(url, timeout) {
-  console.log("watching", url); // Print "watching" message
-  const browser = await firefox.launch();
-  const osList = ['Windows', 'Linux', 'Macintosh'];
-  const os = osList[Math.floor(Math.random() * osList.length)];
-  const context = await browser.newContext({
-    userAgent: generateUserAgent(os),
+async function openPage(userAgent) {
+  return new Promise(async (resolve, reject) => {
+    let browser;
+    try {
+      browser = await firefox.launch({ headless: true });
+      const context = await browser.newContext({ userAgent });
+      const page = await context.newPage();
+      await page.goto(url, { timeout: 60000 });
+      try{
+      await page.click('button[aria-label="Play"]');
+        console.log('clicked');
+      } catch (e) {
+        console.log('no clicked');
+      }
+      await page.waitForTimeout(120000);
+      resolve();
+    } catch (error) {
+      console.error('error:', error);
+      reject(error);
+    } finally {
+      if (browser) {
+        await browser.close();
+      }
+    }
   });
-  const page = await context.newPage();
-  
-  await page.goto(url);
-  const playButton = await page.$('button.play');
-  
-  if (playButton) {
-    await playButton.click();
-  } else {
-    console.log("No play button found.");
-  }
-  
-  setTimeout(async () => {
-    await browser.close();
-    console.log("watching again"); // Print "watching again" message
-    await openBrowser(url, timeout); // Reopen browser after timeout
-  }, timeout * 1000);
 }
 
-async function main() {
-  for (let i = 0; i < numberOfBrowsers; i++) {
-    await openBrowser(url, timeout);
+async function executeInParallel() {
+  const promises = [];
+  for (let i = 0; i < 10; i++) {
+    // إنشاء user agent خاصة بالحاسوب فقط
+    const userAgent = new UserAgent({ deviceCategory: 'desktop' });
+    promises.push(openPage(userAgent.toString()));
+  }
+  await Promise.all(promises).catch(error => {
+    console.error('error run the codes :', error);
+  });
+}
+
+async function repeatForever() {
+  while (true) {
+    await executeInParallel();
+    console.log(`watching again : ${url}`);
   }
 }
 
-main();
+repeatForever();
