4977943f07fa4495ae3fd3a01dc554b2e2866167
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-16 19:11:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 import { firefox } from 'playwright';
 import UserAgent from 'user-agents';
 
-let url = "https://m.youtube.com/watch?v=u5j85Z7EMuM";
+const url = "https://m.youtube.com/watch?v=u5j85Z7EMuM";
 console.log(`watching: ${url}`);
-const userAgent = new UserAgent({ deviceCategory: 'desktop' });
+
+const browsersCount = 5;
+const browserInstances = [];
+
+for (let i = 0; i < browsersCount; i++) {
+  browserInstances.push((async () => {
+    const userAgent = new UserAgent({ deviceCategory: 'desktop' });
+    const browser = await firefox.launch({ headless: true });
+    const context = await browser.newContext({ userAgent: userAgent.toString() });
+    console.log(`Browser ${i+1} userAgent: ${userAgent.toString()}`);
+    const page = await context.newPage();
+    await page.goto(url, { timeout: 0 });
+    console.log(`Browser ${i+1} gone`);
+    try {
+      await page.click('button[aria-label="Play"]');
+      console.log(`Browser ${i+1} clicked`);
+    } catch (e) {
+      console.log(`Browser ${i+1} not clicked`);
+    }
     
-const browser = await firefox.launch({ headless: true });
-const context = await browser.newContext({ userAgent: userAgent.toString() });
-console.log(userAgent.toString());
-const page = await context.newPage();
-await page.goto(url, { timeout: 0 });
-console.log('gone');
-try {
-  await page.click('button[aria-label="Play"]');
-  console.log('clicked');
-} catch (e) {
-  console.log('not clicked');
+    // Wait for 1 minute
+    await new Promise(resolve => setTimeout(resolve, 60000));
+    
+    await browser.close();
+  })());
 }
 
-// Wait for 1 minute
-await new Promise(resolve => setTimeout(resolve, 60000));
-
-await browser.close();
+Promise.all(browserInstances).then(() => {
+  console.log('All browsers have finished their tasks');
+});
